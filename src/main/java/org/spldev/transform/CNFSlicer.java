621bec090cda64eedd6981a0b8954d8b7dca80b6
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:01 +0200

    Updated license headers

--- src/main/java/org/spldev/transform/CNFSlicer.java
+++ src/main/java/org/spldev/transform/CNFSlicer.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Formula-Analysis-Sat4J Lib - Library to analyze propositional formulas with Sat4J.
- * Copyright (C) 2021-2022  Sebastian Krieter
+ * formula-analysis-sat4j - Analysis of propositional formulas using Sat4j
+ * Copyright (C) 2022 Sebastian Krieter
  * 
- * This file is part of Formula-Analysis-Sat4J Lib.
+ * This file is part of formula-analysis-sat4j.
  * 
- * Formula-Analysis-Sat4J Lib is free software: you can redistribute it and/or modify it
+ * formula-analysis-sat4j is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Formula-Analysis-Sat4J Lib is distributed in the hope that it will be useful,
+ * formula-analysis-sat4j is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Formula-Analysis-Sat4J Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with formula-analysis-sat4j. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/formula-analysis-sat4j> for further information.
+ * See <https://github.com/FeatJAR/formula-analysis-sat4j> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.transform;
