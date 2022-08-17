865bfcc1f202dbc55fdd00d59e828f47a2f7dab9
Elias Kuiter <info@elias-kuiter.de>
2022-08-17 17:53:34 +0200

    URLs updated

--- src/main/java/de/featjar/configuration/list/TWiseCoverageMetrics.java
+++ src/main/java/de/featjar/configuration/list/TWiseCoverageMetrics.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with formula-analysis-sat4j. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatJAR/formula-analysis-sat4j> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-formula-analysis-sat4j> for further information.
  */
 package de.featjar.configuration.list;
 
