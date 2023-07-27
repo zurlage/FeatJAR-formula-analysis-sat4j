cef15cec9f653a59842850be2138221249cd4b48
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-07-27 17:45:22 +0200

    Updates header information

--- src/main/java/de/featjar/formula/analysis/sat4j/twise/TWiseStatisticGenerator.java
+++ src/main/java/de/featjar/formula/analysis/sat4j/twise/TWiseStatisticGenerator.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2023 Sebastian Krieter, Tobias Pett
+ * Copyright (C) 2023 Sebastian Krieter
  *
- * This file is part of evaluation-continuous-sampling.
+ * This file is part of FeatJAR-formula-analysis-sat4j.
  *
- * evaluation-continuous-sampling is free software: you can redistribute it and/or modify it
+ * formula-analysis-sat4j is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * evaluation-continuous-sampling is distributed in the hope that it will be useful,
+ * formula-analysis-sat4j is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with evaluation-continuous-sampling. If not, see <https://www.gnu.org/licenses/>.
+ * along with formula-analysis-sat4j. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <> for further information.
+ * See <https://github.com/FeatureIDE/FeatJAR-formula-analysis-sat4j> for further information.
  */
 package de.featjar.formula.analysis.sat4j.twise;
 
