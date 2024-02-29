b2844980ad398c46081980541e01ee5911492319
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-02-29 16:34:23 +0100

    Adds tests

--- src/test/java/de/featjar/formula/analysis/sat4j/Sat4JAnalysesTest.java
+++ src/test/java/de/featjar/formula/analysis/sat4j/Sat4JAnalysesTest.java
@@ -26,19 +26,21 @@ import static de.featjar.formula.structure.Expressions.literal;
 import static de.featjar.formula.structure.Expressions.or;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import de.featjar.AnalysisTest;
 import de.featjar.base.computation.Computations;
 import de.featjar.base.computation.IComputation;
 import de.featjar.formula.analysis.bool.BooleanAssignment;
 import de.featjar.formula.analysis.bool.BooleanAssignmentList;
 import de.featjar.formula.analysis.bool.BooleanClauseList;
 import de.featjar.formula.analysis.bool.BooleanSolution;
+import de.featjar.formula.analysis.bool.ComputeBooleanClauseList;
 import de.featjar.formula.analysis.bool.ComputeBooleanRepresentation;
 import de.featjar.formula.structure.formula.IFormula;
 import de.featjar.formula.transformer.ComputeCNFFormula;
 import de.featjar.formula.transformer.ComputeNNFFormula;
 import org.junit.jupiter.api.Test;
 
-public class Sat4JAnalysesTest {
+public class Sat4JAnalysesTest extends AnalysisTest {
 
     public void getTWiseSample(IFormula formula, int t) {
         ComputeBooleanRepresentation<IFormula, BooleanClauseList> cnf = async(formula)
@@ -48,8 +50,7 @@ public class Sat4JAnalysesTest {
         IComputation<BooleanClauseList> clauses = cnf.map(Computations::getKey);
 
         BooleanSolution solution = await((IComputation<BooleanSolution>) clauses.map(ComputeSolutionSAT4J::new));
-        BooleanAssignment core =
-                await((IComputation<BooleanAssignment>) clauses.map(ComputeCoreDeadVariablesSAT4J::new));
+        BooleanAssignment core = await((IComputation<BooleanAssignment>) clauses.map(ComputeCoreSAT4J::new));
         BooleanAssignmentList atomicSets =
                 await((IComputation<BooleanAssignmentList>) clauses.map(ComputeAtomicSetsSAT4J::new));
         assertNotNull(solution);
@@ -58,7 +59,22 @@ public class Sat4JAnalysesTest {
     }
 
     @Test
-    void test() {
+    void tWiseSampleHasCorrectSize() {
         getTWiseSample(or(literal("x"), literal(false, "y"), literal(false, "z")), 2);
+    }
+
+    @Test
+    void satisfiabilityIsCorrectlyComputed() {
+        testSatisfiability(ComputeBooleanClauseList::new, ComputeSatisfiableSAT4J::new);
+    }
+
+    @Test
+    void coreIsCorrectlyComputed() {
+        testCore(ComputeBooleanClauseList::new, ComputeCoreSAT4J::new);
+    }
+
+    @Test
+    void computedSolutionIsSatisfying() {
+        testSolution(ComputeBooleanClauseList::new, ComputeSolutionSAT4J::new);
     }
 }
