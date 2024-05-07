518818203451ccc95545fa7dd347613605859429
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2024-05-07 12:55:07 +0200

    Revises relative coverage computation
    Signed-off-by Sebastian Krieter <sebastian.krieter@uni-ulm.de>

--- src/main/java/de/featjar/formula/analysis/sat4j/twise/RelativeTWiseCoverageComputation.java
+++ src/main/java/de/featjar/formula/analysis/sat4j/twise/RelativeTWiseCoverageComputation.java
@@ -33,7 +33,9 @@ import de.featjar.formula.analysis.bool.BooleanSolutionList;
 import de.featjar.formula.analysis.combinations.LexicographicIterator;
 import de.featjar.formula.analysis.combinations.LexicographicIterator.Combination;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * Calculates statistics regarding t-wise feature coverage of a set of
@@ -71,7 +73,7 @@ public class RelativeTWiseCoverageComputation extends AComputation<CoverageStati
     }
 
     private ArrayList<ExpandableIntegerList> indexedSolutions;
-    private ArrayList<ExpandableIntegerList> indexedReferenceSolutions;
+    private BitSet[] bitSetReference;
     private ArrayList<Environment> statisticList = new ArrayList<>();
     private int t;
 
@@ -81,28 +83,31 @@ public class RelativeTWiseCoverageComputation extends AComputation<CoverageStati
 
         if (!sample.isEmpty()) {
             BooleanSolutionList referenceSample = REFERENCE_SAMPLE.get(dependencyList);
-            t = T.get(dependencyList);
             assert referenceSample.isEmpty()
                     || sample.get(0).get().size()
                             >= referenceSample.get(0).get().size();
+
+            t = T.get(dependencyList);
             final int size = sample.get(0).get().size();
-            initIndexedLists(sample, referenceSample, size);
+            indexedSolutions = initIndexedLists(sample, size);
+            bitSetReference = initBitSet(referenceSample, size);
+
             final int[] literals = TWiseCoverageComputationUtils.getFilteredLiterals(size, FILTER.get(dependencyList));
-            final boolean[][] masks = TWiseCoverageComputationUtils.getMasks(t);
+            final int[] gray = IntStream.rangeClosed(1, 1 << t)
+                    .map(Integer::numberOfTrailingZeros)
+                    .toArray();
+            gray[gray.length - 1] = 0;
 
             LexicographicIterator.parallelStream(t, literals.length, this::createStatistic)
                     .forEach(combo -> {
-                        for (boolean[] mask : masks) {
-                            for (int k = 0; k < mask.length; k++) {
-                                combo.environment.literals[k] = mask[k]
-                                        ? literals[combo.elementIndices[k]]
-                                        : -literals[combo.elementIndices[k]];
-                            }
-                            if (TWiseCoverageComputationUtils.isCovered(
-                                    indexedReferenceSolutions,
-                                    t,
-                                    combo.environment.literals,
-                                    combo.environment.selectedIndexedSolutions)) {
+                        for (int k = 0; k < t; k++) {
+                            combo.environment.literals[k] = literals[combo.elementIndices[k]];
+                        }
+                        for (int i = 0; i < gray.length; i++) {
+                            if (combinedIndex(size, combo.environment.literals, bitSetReference)
+                                    .isEmpty()) {
+                                combo.environment.statistic.incNumberOfInvalidConditions();
+                            } else {
                                 if (TWiseCoverageComputationUtils.isCovered(
                                         indexedSolutions,
                                         t,
@@ -112,9 +117,9 @@ public class RelativeTWiseCoverageComputation extends AComputation<CoverageStati
                                 } else {
                                     combo.environment.statistic.incNumberOfUncoveredConditions();
                                 }
-                            } else {
-                                combo.environment.statistic.incNumberOfInvalidConditions();
                             }
+                            int g = gray[i];
+                            combo.environment.literals[g] = -combo.environment.literals[g];
                         }
                     });
         }
@@ -123,25 +128,48 @@ public class RelativeTWiseCoverageComputation extends AComputation<CoverageStati
                 .reduce((s1, s2) -> s1.merge(s2)));
     }
 
-    private void initIndexedLists(BooleanSolutionList sample, BooleanSolutionList referenceSample, final int size) {
-        final int indexedListSize = 2 * size;
-        indexedSolutions = new ArrayList<>(indexedListSize);
-        indexedReferenceSolutions = new ArrayList<>(indexedListSize);
-        for (int i = indexedListSize; i >= 0; --i) {
-            indexedSolutions.add(new ExpandableIntegerList());
-            indexedReferenceSolutions.add(new ExpandableIntegerList());
+    private BitSet combinedIndex(final int size, int[] literals, BitSet[] bitSets) {
+        BitSet first = bitSets[literals[0] + size];
+        BitSet bitSet = new BitSet(first.size());
+        bitSet.xor(first);
+        for (int k = 1; k < literals.length; k++) {
+            bitSet.and(bitSets[literals[k] + size]);
         }
-        addConfigurations(sample, indexedSolutions);
-        addConfigurations(referenceSample, indexedReferenceSolutions);
+        return bitSet;
     }
 
-    private void addConfigurations(
-            BooleanSolutionList sample, final ArrayList<ExpandableIntegerList> indexedSolutions) {
+    private ArrayList<ExpandableIntegerList> initIndexedLists(BooleanSolutionList sample, final int size) {
+        ArrayList<ExpandableIntegerList> indexedSolutions = new ArrayList<>(2 * size);
+        for (int i = 2 * size; i >= 0; --i) {
+            indexedSolutions.add(new ExpandableIntegerList());
+        }
         int configurationIndex = 0;
         for (BooleanSolution configuration : sample) {
             TWiseCoverageComputationUtils.addConfigurations(
                     indexedSolutions, configuration.get(), configurationIndex++);
         }
+        return indexedSolutions;
+    }
+
+    private BitSet[] initBitSet(BooleanSolutionList sample, final int size) {
+        final int indexedListSize = 2 * size;
+        BitSet[] indexedSolutions = new BitSet[indexedListSize + 1];
+
+        for (int j = 1; j <= size; j++) {
+            BitSet negIndices = new BitSet(sample.size());
+            BitSet posIndices = new BitSet(sample.size());
+            for (int i = 0; i < sample.size(); i++) {
+                BooleanSolution config = sample.getAll().get(i);
+                if (config.get(j - 1) < 0) {
+                    negIndices.set(i);
+                } else {
+                    posIndices.set(i);
+                }
+            }
+            indexedSolutions[size - j] = negIndices;
+            indexedSolutions[j + size] = posIndices;
+        }
+        return indexedSolutions;
     }
 
     private Environment createStatistic(Combination<Environment> combo) {
